331455a35238a639e5a4a97e0b039e0883f7e411
yuebon <cqinwn@qq.com>
2020-10-23 23:20:43 +0800

    优化集合返回值问题

---
+++ Yuebon.NetCore/Yuebon.Commons/Helpers/UpperFirstCaseNamingPolicy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+using Yuebon.Commons.Extend;
+
+namespace Yuebon.Commons.Helpers
+{
+    /// <summary>
+    /// 首字母大写
+    /// </summary>
+    public class UpperFirstCaseNamingPolicy:JsonNamingPolicy
+    {
+        public override string ConvertName(string name) =>
+            name.UpperFirst();
+    }
+}

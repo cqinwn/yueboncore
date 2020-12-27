b997160cfc9b8de7739715d2ee0b7fd902887177
yuebon <cqinwn@qq.com>
2020-12-27 15:54:14 +0800

    优化登录超时提醒

--- VueUI/src/utils/request.js
+++ VueUI/src/utils/request.js
@@ -33,7 +33,7 @@ service.interceptors.response.use(
     if (res.ErrCode !== '0') {
       if (res.ErrCode === '40000' || res.ErrCode === '40001' || res.ErrCode === '40002' || res.ErrCode === '40004' || res.ErrCode === '40005' || res.ErrCode === '40008') {
         // to re-login
-        MessageBox.confirm('登录超时，请重新登录', '登录提示', {
+        MessageBox.confirm('登录状态已过期，您可以继续留在该页面，或者重新登录', '系统提示', {
           confirmButtonText: '重新登录',
           cancelButtonText: '取消',
           type: 'warning'

d522bdd9402290ea19875772641a8abc1aff62d6
yuebon <cqinwn@qq.com>
2021-01-28 17:33:00 +0800

    优化前端错误日志提示

--- VueUI/src/utils/request.js
+++ VueUI/src/utils/request.js
@@ -68,6 +68,15 @@ service.interceptors.response.use(
     }
   },
   error => {
+    console.log('err' + error)
+    let { message } = error
+    if (message === 'Network Error') {
+      message = '后端接口连接异常'
+    } else if (message.includes('timeout')) {
+      message = '系统接口请求超时'
+    } else if (message.includes('Request failed with status code')) {
+      message = '系统接口' + message.substr(message.length - 3) + '异常'
+    }
     Message({
       message: error.message,
       type: 'error',

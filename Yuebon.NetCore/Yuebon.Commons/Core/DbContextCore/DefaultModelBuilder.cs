a5ce19f5e91719c3f7da1567f8690086e9fbe341
qingwenchen <cqinwn@qq.com>
2021-11-26 13:53:46 +0800

    更新包

--- Yuebon.NetCore/Yuebon.Commons/Core/DbContextCore/DefaultModelBuilder.cs
+++ Yuebon.NetCore/Yuebon.Commons/Core/DbContextCore/DefaultModelBuilder.cs
@@ -6,12 +6,24 @@ using System.Text;
 
 namespace Yuebon.Commons.EfDbContext
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class DefaultModelBuilder : ModelBuilder
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="conventions"></param>
         public DefaultModelBuilder(ConventionSet conventions) : base(conventions)
         {
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TEntity"></typeparam>
+        /// <param name="configuration"></param>
+        /// <returns></returns>
         public override ModelBuilder ApplyConfiguration<TEntity>(IEntityTypeConfiguration<TEntity> configuration)
         {
             return base.ApplyConfiguration(configuration);

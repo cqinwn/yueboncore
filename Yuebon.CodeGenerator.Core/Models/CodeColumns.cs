2b245a982035f986061d0f5b1c72cc2578ca21d1
qingwenchen <cqinwn@qq.com>
2022-05-18 13:26:42 +0800

    优化菜单修改bug

---
+++ Yuebon.CodeGenerator.Core/Models/CodeColumns.cs
@@ -0,0 +1,54 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yuebon.Commons.Models;
+
+namespace Yuebon.CodeGenerator.Models
+{
+    [SugarTable("Sys_CodeColumns", "表字段")]
+    [Serializable]
+    public class CodeColumns : BaseEntity
+    {
+        /// <summary>
+        /// 实体属性名称
+        /// </summary>
+        public string ClassProperName { get; set; }
+        /// <summary>
+        /// 表字段名称
+        /// </summary>
+        public string DbColumnName { get; set; }
+        /// <summary>
+        /// 是否必填
+        /// </summary>
+        public bool Required { get; set; }
+        /// <summary>
+        /// 是否自增长
+        /// </summary>
+        public bool IsIdentity { get; set; }
+        /// <summary>
+        /// 是否是主键
+        /// </summary>
+        public bool IsPrimaryKey { get; set; }
+        /// <summary>
+        /// 描述
+        /// </summary>
+        [SugarColumn(IsNullable = true)]
+        public string Description { get; set; }
+        /// <summary>
+        /// 数据类型
+        /// </summary>
+        public string CodeType { get; set; }
+        /// <summary>
+        /// 所属表
+        /// </summary>
+        public long CodeTableId { get; set; }
+        /// <summary>
+        /// 默认值
+        /// </summary>
+        [SugarColumn(IsNullable = true)]
+        public string DefaultValue { get; set; }
+    }
+}
---
+++ Yuebon.CodeGenerator.Core/Models/Database.cs
@@ -0,0 +1,42 @@
+﻿using SqlSugar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Yuebon.Commons.Models;
+
+namespace Yuebon.CodeGenerator.Models
+{
+    /// <summary>
+    /// 数据库信息
+    /// </summary>
+    [SugarTable("Sys_Database", "数据库信息")]
+    [Serializable]
+    public class Database : BaseEntity
+    {
+        /// <summary>
+        /// 描述
+        /// </summary>
+        public string Desc { get; set; }
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
+        [SugarColumn(ColumnDescription = "数据库连接",Length =600)]
+        public string Connection { get; set; }
+        /// <summary>
+        /// 数据库类型
+        /// </summary>
+        public DbType DbType { get; set; }
+        /// <summary>
+        /// 最后修改时间
+        /// </summary>
+        [SugarColumn(ColumnDescription = "最后修改时间")]
+        public virtual DateTime? LastModifyTime { get; set; }
+        /// <summary>
+        /// 删除标志
+        /// </summary>
+        [SugarColumn(ColumnDescription = "删除标志")]
+        public virtual bool? DeleteMark { get; set; }
+    }
+}

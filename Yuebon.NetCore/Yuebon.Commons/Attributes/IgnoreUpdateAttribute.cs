802569a8a7d867cd2e7429b20d64d6b555ec328a
qingwenchen <cqinwn@qq.com>
2023-03-22 00:40:00 +0800

    是否忽略更新

---
+++ Yuebon.NetCore/Yuebon.Commons/Attributes/IgnoreUpdateAttribute.cs
@@ -0,0 +1,11 @@
+﻿namespace Yuebon.Commons.Attributes
+{
+
+    /// <summary>
+    /// 忽略更新种子数据特性
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true, Inherited = true)]
+    public class IgnoreUpdateAttribute : Attribute
+    {
+    }
+}
